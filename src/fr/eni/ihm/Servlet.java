64e1d2ba319831202be54d2d0ba3ebdb3aeb72ca
Antho-cs <68584530+Antho-cs@users.noreply.github.com>
2020-08-17 12:42:23 +0200

    Initial commit

---
+++ src/fr/eni/ihm/Servlet.java
@@ -0,0 +1,29 @@
+package fr.eni.ihm;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet
+ */
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/Page_acceuil.jsp").forward(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
+ 
