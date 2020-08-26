a6c30e6f80514206a6362bf87d8143459642ade5
Antho-cs <68584530+Antho-cs@users.noreply.github.com>
2020-08-26 15:34:37 +0200

    Test

--- src/fr/eni/dal/Jdbc/EnchereDAOJdbcImpl.java
+++ src/fr/eni/dal/Jdbc/EnchereDAOJdbcImpl.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import fr.eni.bo.Enchere;
+import fr.eni.bo.Utilisateur;
 import fr.eni.dal.ConnectionProvider;
 import fr.eni.dal.DALException;
 import fr.eni.dal.EnchereDAO;
@@ -15,12 +16,7 @@ public class EnchereDAOJdbcImpl implements EnchereDAO {
 
 	private static final String SQL_INSERT = "insert into encheres (no_enchere, date_enchere, montant_enchere, no_article, no_utilisateur) values (?,?,?,?,?)";
 	private static final String SQL_UPDATE = "update encheres set no_enchere = ?,date_enchere = ?, montant_enchere = ?, no_article = ? ,no_utilisateur = ?";
-
-	// private static final String SQL_DELETE = "delete
-	// pseudo,nom,prenom,email,telephone,rue,code_postal,ville,mot_de_passe from
-	// utilisateurs where no_utilisateur = ?";
-	// private static final String SQL_SELECTBYID = "select * from utilisateurs
-	// where no_utilisateur = ?";
+	private static final String SQL_SELECTBYID = "select * from encheres where no_enchere = ?";
 
 	public Enchere insertNewEnchere(Enchere enchere) throws DALException {
 
@@ -64,43 +60,36 @@ public class EnchereDAOJdbcImpl implements EnchereDAO {
 
 	}
 
-	public void updateEnchere(Enchere enchere) throws DALException {
+	public Enchere selectByIdEnchere(int no_Enchere) throws DALException {
+		
 		Connection cnx = null;
 		PreparedStatement pStmt = null;
+		ResultSet rs = null;
+		Enchere enchere = null;
 
 		try {
 			cnx = ConnectionProvider.getConnection();
 
-			pStmt = cnx.prepareStatement(SQL_UPDATE);
+			pStmt = cnx.prepareStatement(SQL_SELECTBYID);
 
-			pStmt.setInt(1, enchere.getNoEnchere());
-			pStmt.setLocalDateTime(2, enchere.getDateEnchere());
-			pStmt.setInt(3, enchere.getMontantEnchere());
-			pStmt.setArticleVendu(4, enchere.getNoArticle());
-			pStmt.setUtlilisateur(5, enchere.getNoUtilisateur());
+			pStmt.setInt(1, no_Enchere);
 
-			pStmt.executeUpdate();
-			ResultSet rs = pStmt.getGeneratedKeys();
-			if (rs.next()) {
-				System.out.println(rs.getInt(1));
-				enchere.setNoEnchere(rs.getInt(1));
-
-			}
-
+			rs = pStmt.executeQuery();
+			rs.next();
+			enchere = new Enchere(
+					no_Enchere,
+					rs.getInt(""),
+					rs.getString(""),
+					rs.getString(""),
+					rs.getString("");
+				
+		
 		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-			throw new DALException("Erreur lors de la mise à jour de l'enchère : " + enchere, e);
-
-		} finally {
-
-			try {
-				if (cnx != null) {
-					cnx.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+			throw new DALException("selectById failed - id = " + no_utilisateur, e);
 		}
 
+		return enchere;
+
 	}
+
 }
