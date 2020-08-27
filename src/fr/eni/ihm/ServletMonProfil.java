83c14c4a164724763a10c68e619b3335cdc4ac8d
Antho-cs <68584530+Antho-cs@users.noreply.github.com>
2020-08-27 21:55:08 +0200

    Final

--- src/fr/eni/ihm/ServletMonProfil.java
+++ src/fr/eni/ihm/ServletMonProfil.java
@@ -1,6 +1,7 @@
 package fr.eni.ihm;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,12 +24,14 @@ public class ServletMonProfil extends HttpServlet {
 	HttpSession session;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		try {
-			session=request.getSession(false);  
+			session = request.getSession(false);
 			user = (Utilisateur) session.getAttribute("user");
 			request.setAttribute("user", user);
 			request.getRequestDispatcher("/WEB-INF/monProfil.jsp").forward(request, response);
@@ -37,17 +40,17 @@ public class ServletMonProfil extends HttpServlet {
 			response.sendRedirect("Servlet");
 		}
 
-
-
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if(request.getParameter("btn").equalsIgnoreCase("enregistrer")) {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		if (request.getParameter("btn").equalsIgnoreCase("enregistrer")) {
 			try {
-				if(user.getMot_de_passe().equals(request.getParameter("motDePasse").trim())) {
+				if (user.getMot_de_passe().equals(request.getParameter("motDePasse").trim())) {
 					user.setPseudo(request.getParameter("pseudo"));
 					user.setEmail(request.getParameter("email"));
 					user.setNom(request.getParameter("nom"));
@@ -56,7 +59,8 @@ public class ServletMonProfil extends HttpServlet {
 					user.setTelephone(request.getParameter("telephone"));
 					user.setVille(request.getParameter("ville"));
 					user.setRue(request.getParameter("rue"));
-					if(!request.getParameter("nouveauMotDePasse").equals("")) {
+					user.setCredit(100);
+					if (!request.getParameter("nouveauMotDePasse").equals("")) {
 						user.setMot_de_passe(request.getParameter("nouveauMotDePasse"));
 					}
 					mgr.updateUser(user);
@@ -64,10 +68,10 @@ public class ServletMonProfil extends HttpServlet {
 			} catch (BLLException e) {
 				e.printStackTrace();
 			}
-		}else if(request.getParameter("btn").equalsIgnoreCase("supprimer")) {
+		} else if (request.getParameter("btn").equalsIgnoreCase("supprimer")) {
 			try {
 				mgr.deleteUser(user.getNo_utilisateur());
-				HttpSession session=request.getSession(false);  
+				HttpSession session = request.getSession(false);
 				session.invalidate();
 			} catch (BLLException e) {
 				e.printStackTrace();
